2e0faf9ef84817fb9c38fe85a61a70663a82c715
fabiantheblind <icke@fabianmoronzirfas.me>
2014-01-30 13:26:03 +0100

    npm bower grunt

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+/*global module:false*/
+module.exports = function(grunt) {
+    // load all grunt tasks matching the `grunt-*` pattern
+    require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+
+    'bower-install': {
+
+  target: {
+
+    // Point to the files that should be updated when
+    // you run `grunt bower-install`
+    src: ['index.html'],
+
+    // Optional:
+    // ---------
+    cwd: '',
+    ignorePath: '',
+    exclude: [],
+    fileTypes: {}
+  }
+},
+    // Task configuration.
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'qunit']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  // grunt.loadNpmTasks('grunt-contrib-qunit');
+  // grunt.loadNpmTasks('grunt-contrib-jshint');
+  // grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'qunit']);
+
+};
